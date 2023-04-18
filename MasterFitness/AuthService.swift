69b81cd6facf4f3ed2b4bf8840d23ba4aff9809c
Chamidu <Chamidu.nagahawatta1@gmail.com>
2023-04-18 19:51:00 +0530

    feat(MasterFitness) : Firebase Init

---
+++ MasterFitness/AuthService.swift
@@ -0,0 +1,16 @@
+//
+//  AuthService.swift
+//  MasterFitness
+//
+//  Created by Chamidu on 18/04/2023.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthService {
+
+    public static let shared = AuthService()
+    private init() {}
+
+}
