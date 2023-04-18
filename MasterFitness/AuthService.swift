acd93d66fb93bd0d5276cebc10b536180034313e
Chamidu <Chamidu.nagahawatta1@gmail.com>
2023-04-18 19:57:28 +0530

    feat(Auth) : Register User Init

--- MasterFitness/AuthService.swift
+++ MasterFitness/AuthService.swift
@@ -7,10 +7,49 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestore
 
 class AuthService {
 
     public static let shared = AuthService()
     private init() {}
 
+    /// Register the User
+       /// - Parameters:
+       ///   - userRequest: The User Info (email, password, username)
+       ///   - completion: A completion with two values...
+       ///   - Bool: wasRegistered - Determins if the user was registered and saved in DB
+       ///   - Error?: An Optional Error if Firebase throws one
+       public func registerUser(with userRequest: RegisterUserRequest, completion: @escaping(Bool, Error?) ->Void){
+           let username = userRequest.username
+           let email = userRequest.email
+           let password = userRequest.password
+
+           Auth.auth().createUser(withEmail: email, password: password) { Result, error in
+               if let error = error {
+                   completion(false, error)
+                   return
+               }
+               guard let resultUser = Result?.user else {
+                   completion(false, nil)
+                   return
+               }
+
+               let db = Firestore.firestore()
+
+               db.collection("users")
+                   .document(resultUser.uid)
+                   .setData([
+                       "username": username,
+                       "email": email,
+                       "isOnBoardingDone": false,
+                   ]) { error in
+                       if let error = error {
+                           completion(false, error)
+                           return
+                       }
+                       completion(true, nil)
+                   }
+           }
+       }
 }
---
+++ MasterFitness/Models/RegisterUserRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterUserRequest.swift
+//  MasterFitness
+//
+//  Created by Chamidu on 18/04/2023.
+//
+
+import Foundation
+
+struct RegisterUserRequest {
+    let username: String
+    let email: String
+    let password: String
+}
